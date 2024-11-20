2f9532d6c9044f7f47767caaa0ed2c1e62fc5c30
Robbie Khaddaj <rkadaj@outlook.com>
2024-11-19 22:44:28 -0500

    update vscode extension

--- lib/vs_code.rb
+++ lib/vs_code.rb
@@ -5,6 +5,7 @@ class VSCode < Base
   sig { override.void }
   def run
     return unless macos?
+
     Link.new(source: "vscode/settings.json",
              target: "~/Library/Application Support/Code/User/settings.json").run
     Link.new(source: "vscode/keybindings.json",
